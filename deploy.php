801908f7651fe59171087e231865c815de619275
Maksim Khortov <xoptov@mail.ru>
2019-04-11 16:46:05 +0300

    Реализовал деплоймент на stage сервер.

---
+++ deploy.php
@@ -0,0 +1,36 @@
+<?php
+namespace Deployer;
+require 'recipe/symfony.php';
+
+// Configuration
+
+set('ssh_type', 'native');
+set('ssh_multiplexing', true);
+
+set('git_tty', true);
+
+set('repository', 'git@bitbucket.org:phoenix-soft/edward-webapp.git');
+
+set('bin_dir', 'bin');
+set('var_dir', 'var');
+
+set('shared_dirs', ['var/logs']);
+add('shared_files', []);
+set('writable_dirs', ['var/cache', 'var/logs']);
+
+// Servers
+
+server('stage', '94.130.178.198')
+    ->user('xoptov')
+    ->set('deploy_path', '/var/www/stage.edward-lead.ru')
+    ->pty(true);
+
+
+// Tasks
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'database:migrate');
