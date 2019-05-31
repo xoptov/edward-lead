a346a4de3fd3aaf007bdefabfa20359da24c8a27
Maksim Khortov <xoptov@mail.ru>
2019-05-31 13:18:45 +0300

    Подготовил экшен для приёма картинок.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -16,5 +17,17 @@ class DefaultController extends Controller
     public function indexAction(): Response
     {
         return $this->render('@App/Default/index.html.twig');
+    }
+
+    /**
+     * @Route("/upload/image", name="app_upload_image", methods={"POST"})
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function uploadImageAction(Request $request): Response
+    {
+        return new Response('ok');
     }
 }
