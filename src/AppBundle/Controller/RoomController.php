4937fb4782124df45ac3ad0ee71f440a0090ed51
Maksim Khortov <xoptov@mail.ru>
2019-07-09 16:24:11 +0300

    Реализовал инвайт в комнаты, с черновым дизайном

--- src/AppBundle/Controller/RoomController.php
+++ src/AppBundle/Controller/RoomController.php
@@ -40,21 +40,100 @@ class RoomController extends Controller
         $form->handleRequest($request);
 
         if ($form->isValid()) {
+
             /** @var Room $data */
             $data = $form->getData();
             $data
                 ->setOwner($this->getUser())
                 ->setEnabled(true);
-            $roomManager->updateInviteToken($data);
 
             $this->entityManager->persist($data);
+
+            try {
+                $roomManager->joinInRoom($data, $this->getUser());
+            } catch (\Exception $e) {
+                $this->addFlash('error', $e->getMessage());
+
+                return $this->redirectToRoute('app_profile');
+            }
+
+            $roomManager->updateInviteToken($data);
             $this->entityManager->flush();
 
             $this->addFlash('success', 'Новая комната успешно создана');
 
-            return $this->render('@App/Default/index.html.twig');
+            return $this->redirectToRoute('app_profile');
         }
 
         return $this->render('@App/Room/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/room/invite/{token}", name="app_invite_room", methods={"GET"})
+     *
+     * @param string $token
+     *
+     * @return Response
+     */
+    public function inviteAction(string $token): Response
+    {
+        $room = $this->entityManager->getRepository(Room::class)->findOneBy([
+            'inviteToken' => $token,
+            'enabled' => true
+        ]);
+
+        if (!$room) {
+            $this->addFlash('error', 'Невалидное приглашение в комнату');
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('@App/Room/invite.html.twig', [
+            'room' => $room
+        ]);
+    }
+
+    /**
+     * @Route("/invite/accept/{token}", name="app_accept_invite", methods={"GET"})
+     *
+     * @param string                 $token
+     * @param RoomManager            $roomManager
+     *
+     * @return Response
+     */
+    public function acceptInviteAction(string $token, RoomManager $roomManager): Response
+    {
+        $room = $this->entityManager->getRepository(Room::class)->findOneBy([
+            'inviteToken' => $token,
+            'enabled' => true
+        ]);
+
+        if (!$room) {
+            $this->addFlash('error', 'Невалидное приглашение в группу');
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        try {
+            $roomManager->joinInRoom($room, $this->getUser());
+            $roomManager->updateInviteToken($room);
+            $this->entityManager->flush();
+        } catch (\Exception $e) {
+            $this->addFlash('error', $e->getMessage());
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return new Response('Вы вступили в приглашённую группу');
+    }
+
+    /**
+     * @Route("/invite/reject", name="app_reject_invite", methods={"GET"})
+     *
+     * @return Response
+     */
+    public function rejectInviteAction(): Response
+    {
+        return new Response('Вы отказались вступить в группу');
     }
 }
