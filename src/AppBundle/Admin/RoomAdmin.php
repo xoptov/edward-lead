b61429420a036d02de36d35a71e4c7d22572a2fe
Maksim Khortov <xoptov@mail.ru>
2019-07-10 12:00:20 +0300

    Просмотр информации о комнате в админке.

--- src/AppBundle/Admin/RoomAdmin.php
+++ src/AppBundle/Admin/RoomAdmin.php
@@ -10,7 +10,6 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use AppBundle\Admin\Field\MoneyFieldDescription;
-use Sonata\AdminBundle\Show\ShowMapper;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use AppBundle\Form\Type\DataTransformer\MoneyTransformer;
 
