ca7536218ce3e8876d31ab03dbd9552d1097cdf5
Maksim Khortov <xoptov@mail.ru>
2019-08-14 12:09:59 +0300

    Добавил списки отображения лидов и членов комнаты в просмотр комнаты в админке.

--- src/AppBundle/Controller/Admin/RoomController.php
+++ src/AppBundle/Controller/Admin/RoomController.php
@@ -2,24 +2,36 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Entity\Lead;
 use AppBundle\Entity\Room;
-use AppBundle\Entity\User;
+use AppBundle\Entity\Member;
 use AppBundle\Service\RoomManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Sonata\AdminBundle\Controller\CRUDController;
+use Sonata\AdminBundle\Admin\FieldDescriptionCollection;
 
 class RoomController extends CRUDController
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
     /**
      * @var RoomManager
      */
     private $roomManager;
 
     /**
-     * @param RoomManager $roomManager
+     * @param EntityManagerInterface $entityManager
+     * @param RoomManager            $roomManager
      */
-    public function __construct(RoomManager $roomManager)
-    {
+    public function __construct(
+        RoomManager $roomManager,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->entityManager = $entityManager;
         $this->roomManager = $roomManager;
     }
 
@@ -34,5 +46,30 @@ class RoomController extends CRUDController
         if ($owner) {
             $this->roomManager->joinInRoom($object, $owner);
         }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function preShow(Request $request, $object)
+    {
+        $this->admin->setSubject($object);
+
+        $fields = $this->admin->getShow();
+        \assert($fields instanceof FieldDescriptionCollection);
+
+        $members = $this->entityManager->getRepository(Member::class)
+            ->getByRooms([$object]);
+
+        $leads = $this->entityManager->getRepository(Lead::class)
+            ->findBy(['room' => $object], ['createdAt' => 'DESC', 'updatedAt' => 'DESC']);
+
+        return $this->renderWithExtraParams('@App/CRUD/room_show.html.twig', [
+            'action' => 'show',
+            'object' => $object,
+            'elements' => $fields,
+            'members' => $members,
+            'leads' => $leads
+        ], null);
     }
 }
