ad67c28334d7be8422715f9f02ed5cf447f97716
Maksim Khortov <xoptov@mail.ru>
2019-11-05 12:19:41 +0300

    Добавил проверку на деактивацию комнаты если в комнате есть люди.

--- src/AppBundle/Repository/MemberRepository.php
+++ src/AppBundle/Repository/MemberRepository.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\Room;
+use Doctrine\DBAL\DBALException;
 use Doctrine\ORM\EntityRepository;
 
 class MemberRepository extends EntityRepository
@@ -21,5 +23,28 @@ class MemberRepository extends EntityRepository
             ->getQuery();
 
         return $query->getResult();
+    }
+
+    /**
+     * @param Room $room
+     *
+     * @return int
+     *
+     * @throws DBALException
+     */
+    public function getCountInRoom(Room $room): int
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = 'SELECT COUNT(id) FROM member WHERE room_id = :room_id';
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue('room_id', $room->getId());
+
+        if ($stmt->execute() && $stmt->rowCount()) {
+            return $stmt->fetchColumn();
+        }
+
+        return 0;
     }
 }
