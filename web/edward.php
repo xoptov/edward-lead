37a2c21b1b49637dc11359addd4eaad403bbb21d
Maksim Khortov <xoptov@mail.ru>
2019-11-07 13:34:54 +0300

    Вобщем подкрутил немного файл пересылки данных формы, надо допилить ответ success в json и ошибки в json.

---
+++ web/edward.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * @author Maksim Khortov <xoptov@mail.ru>
+ * @version 1.0.0
+ */
+
+define('DEBUG_MODE', false);
+
+define('API_KEY', '77826e8bc72600cc6be514fc9d6c507c70253c76'); // Необходимо указать ключь API внутри ''
+define('ROOM_ID', 1057); // Идентификатор комнаты в которую будет добавляться лид.
+
+//define('API_URL', 'https://cabinet.edward-lead.ru/api/v1/lead');
+define('API_URL', 'http://172.19.0.1:8000/api/v1/lead');
+
+ini_set('error_reporting', E_ERROR);
+
+if (!extension_loaded('json')) {
+    sendResponse('Необходимо подключить json расширение к php', 500, ['Content-Type: text/html; charset=UTF-8']);
+}
+
+if (isset($_GET['check']) && $_GET['check']) {
+    $checks = [
+        'curl' => extension_loaded('curl'),
+    ];
+    sendResponse(json_encode($checks));
+}
+
+if (!extension_loaded('curl')) {
+    sendResponse('Необходимо подключить curl расширение к php', 500, ['Content-Type: text/html; charset=UTF-8']);
+}
+
+checkConfigs();
+
+$formFields = ['name', 'phone', 'hasAgreement'];
+$errors = [];
+
+foreach ($formFields as $field)
+{
+    $validateFunction = 'validate' . $field;
+    if (function_exists($validateFunction)) {
+        call_user_func($validateFunction, $errors);
+    }
+}
+
+if (count($errors)) {
+    $content = ['errors' => $errors];
+    sendResponse(json_encode($content), 400);
+}
+
+sendLeadToPlatform($formFields);
+
+function validatePhone(array &$errors)
+{
+    if (empty($_POST['phone'])) {
+        $errors[] = ['phone' => 'Необходимо указать номер телефона'];
+    }
+}
+
+function sendLeadToPlatform(array &$formFields)
+{
+    $dataForSubmit = [];
+
+    foreach ($formFields as $field) {
+        if (isset($_POST[$field])) {
+            $dataForSubmit[$field] = $_POST[$field];
+        }
+    }
+
+    $dataForSubmit['room'] = ROOM_ID;
+    $postData = http_build_query($dataForSubmit);
+
+    $headers = [
+        'Content-Type: application/x-www-form-urlencoded; charset=UTF-8',
+        'X-Auth-Token: ' . API_KEY
+    ];
+
+    if (DEBUG_MODE) {
+        $headers[] = 'Cookie: XDEBUG_SESSION=PHPSTORM';
+    }
+
+    $ch = curl_init(API_URL);
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+
+    curl_close($ch);
+
+    //todo: тут нужно проверять какой ответ и если ошибка то отправлять ответ с ошибкой.
+
+    return $response;
+}
+
+function sendResponse(string $content, int $code = 200, array $headers = [])
+{
+    http_response_code($code);
+    header('Content-Type: application/json; charset=UTF-8');
+    foreach ($headers as $header) {
+        header($header, true);
+    }
+    echo $content;
+    exit();
+}
+
+function checkConfigs()
+{
+    if (empty(API_KEY)) {
+        $content = ['error' => 'Необходимо указать API_KEY'];
+        sendResponse(json_encode($content), 500);
+    }
+
+    if (empty(API_URL)) {
+        $content = ['error' => 'Необходимо указать API_URL'];
+        sendResponse(json_encode($content), 500);
+    }
+
+    if (empty(ROOM_ID)) {
+        $content = ['error' => 'Необходимо указать ROOM_ID'];
+        sendResponse(json_encode($content), 500);
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        $content = ['error' => 'Данные необходимо отправлять методом POST'];
+        sendResponse(json_encode($content), 400);
+    }
+}
