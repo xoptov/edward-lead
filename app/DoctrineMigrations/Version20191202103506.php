6f47495303a2665128e9493af51042821f9d2bef
viktor <atsutavictor.dev@gmail.com>
2019-12-02 17:16:43 +0300

    add web push dunctional

--- app/DoctrineMigrations/Version20191202103506.php
+++ app/DoctrineMigrations/Version20191202103506.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191128122411 extends AbstractMigration
+final class Version20191202103506 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
--- src/NotificationBundle/Resources/public/js/web_push_token_handler.js
+++ src/NotificationBundle/Resources/public/js/web_push_token_handler.js
@@ -1,48 +1,51 @@
 ready(function(){
 
-    runEsputnik()
+    (async () => {
 
-    var DBOpenRequest = window.indexedDB.open("es_push_integration", 4);
+        const dbName = 'es-push-integration'
+        const storeName = 'integration-data'
+        const key = 'SID'
+        const version = 1
 
+        const db = await openDB(dbName, version)
+        const item = await db.transaction(storeName).objectStore(storeName).get(key)
+
+        item.onsuccess = (e) => {
+
+            const token = e.target.result.value.sid;
+
+            if(isTokenExpired()){
+
+                var xhr = new XMLHttpRequest();
+                var url = "/api/notifications/push";
+                xhr.open("POST", url, true);
+                xhr.setRequestHeader("Content-Type", "application/json");
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+
+                        setExpiration()
+
+                    }
+                };
+
+                xhr.onerror = function () {
+                    console.error('Some error when sending web push token to server')
+                }
+
+                var data = JSON.stringify({token});
+                xhr.send(data);
+
+            }
+
+        }
+
+        item.onerror = (e) => {
+            console.error('Some error when getting web push token from IndexDb')
+        }
+
+    })()
 
 });
-
-
-var DBOpenRequest = window.indexedDB.open("toDoList", 4);
-
-DBOpenRequest.onsuccess = function(event) {
-
-    db = DBOpenRequest.result;
-    console.log(db)
-};
-//
-// function getData() {
-//     // open a read/write db transaction, ready for retrieving the data
-//     var transaction = db.transaction(["toDoList"], "readwrite");
-//
-//     // report on the success of the transaction completing, when everything is done
-//     transaction.oncomplete = function(event) {
-//         note.innerHTML += '<li>Transaction completed.</li>';
-//     };
-//
-//     transaction.onerror = function(event) {
-//         note.innerHTML += '<li>Transaction not opened due to error: ' + transaction.error + '</li>';
-//     };
-//
-//     // create an object store on the transaction
-//     var objectStore = transaction.objectStore("toDoList");
-//
-//     // Make a request to get a record by key from the object store
-//     var objectStoreRequest = objectStore.get("Walk dog");
-//
-//     objectStoreRequest.onsuccess = function(event) {
-//         // report the success of our request
-//         note.innerHTML += '<li>Request successful.</li>';
-//
-//         var myRecord = objectStoreRequest.result;
-//     };
-//
-// };
 
 
 function ready(callback){
@@ -56,16 +59,77 @@ function ready(callback){
         });
 }
 
-// TODO remove
-function runEsputnik() {
+//////// TOKEN EXPIRATION /////////
 
-    (function(i,s,o,g,r,a,m){
-            i["esSdk"] = r;
-            i[r] = i[r] || function() {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, a=s.createElement(o), m=s.getElementsByTagName(o)[0]; a.async=1; a.src=g;
-            m.parentNode.insertBefore(a,m)}
-    ) (window, document, "script", "https://esputnik.com/scripts/v1/public/scripts?apiKey=eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI0NTI0ZWZhYTJkYzI2MGRmYTM4YTE1NDBlMWFhYjE0N2Q1OTAzNzBiM2Y0Zjk3ODQwYmE4MGU2ZGM3YzEwMDRmMjJhOGU1MzE1ZmJlYTIyZTBhMDMzY2FhODQ3Yjg2NTQ1MGFhYTM0NjEwNjUzNGMxZTcyMjRhOTQ2NjVmYmM0NGJkZDhlYjZkNmIyMDc0NWFhNDY0YjcyNWIzODg0YjEyMDI4ZGVjOTY5YTU3In0.we1MGdYaaQ-OGYw9cdmvx-pc6s4ILdtXcIN3BrShFUS-VABz37Xrn2gQp8Affq-u0qxg8w51T0MGZtzhEsntZg&domain=24A4E9F1-04AB-4E99-9D21-E5D12739B62F", "es");
-    es("pushOn")
+function isTokenExpired() {
 
+    var date = new Date().toLocaleDateString();
+
+    if( localStorage.WEB_PUSH_TOKEN_DATE_SAT == date )
+        return false;
+
+    return true;
 }
+
+function setExpiration() {
+
+    localStorage.WEB_PUSH_TOKEN_DATE_SAT = new Date().toLocaleDateString()
+}
+
+//////// TOKEN EXPIRATION /////////
+
+
+//////// DB /////////
+
+function openDB(name, version, { blocked, upgrade, blocking } = {}) {
+    var request = indexedDB.open(name, version);
+    if (upgrade) {
+        request.addEventListener('upgradeneeded', event => {
+            upgrade(wrap(request.result), event.oldVersion, event.newVersion, wrap(request.transaction));
+        });
+    }
+
+    return  wrap(request);
+}
+
+function promisifyRequest(request) {
+    const promise = new Promise((resolve, reject) => {
+        const unlisten = () => {
+            request.removeEventListener('success', success);
+            request.removeEventListener('error', error);
+        };
+        const success = () => {
+            resolve(wrap(request.result));
+            unlisten();
+        };
+        const error = () => {
+            reject(request.error);
+            unlisten();
+        };
+        request.addEventListener('success', success);
+        request.addEventListener('error', error);
+    });
+    promise.
+    then(value => {
+        // Since cursoring reuses the IDBRequest (*sigh*), we cache it for later retrieval
+        // (see wrapFunction).
+        if (value instanceof IDBCursor) {
+            cursorRequestMap.set(value, request);
+        }
+        // Catching to avoid "Uncaught Promise exceptions"
+    }).
+    catch(() => {});
+
+    return promise;
+}
+
+
+function wrap(value) {
+
+    if (value instanceof IDBRequest)
+        return promisifyRequest(value);
+
+    return value;
+}
+
+//////// DB /////////
