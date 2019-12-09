8d5400c1d40e979a0bd1f55e8d927bdbd92e635a
Maksim Khortov <xoptov@mail.ru>
2019-12-09 22:11:29 +0300

    Добавил на фронтенд скрипт показывания модалок, осталось сделать отправку запроса на бэкенд

--- src/AppBundle/Resources/public/v3/js/offer/list.js
+++ src/AppBundle/Resources/public/v3/js/offer/list.js
@@ -1,3 +1,67 @@
-new Vue({
-    el: ''
+const offersList = new Vue({
+    el: '#offers-list',
+    methods: {
+        onPublicOfferClick() {
+            this.$emit('public-offer-clicked');
+        },
+        onNeedAdvertisersClick() {
+            this.$emit('need-advertisers-clicked');
+        }
+    }
 });
+
+const modals = new Vue({
+    el: '#modals',
+    data: {
+        publicOfferModal: false,
+        needAdvertisersModal: false,
+        requestResultModal: false,
+        requestPending: false
+    },
+    computed: {
+        isPublicOfferModalShow() {
+            return this.publicOfferModal;
+        },
+        isNeedAdvertisersModalShow() {
+            return this.needAdvertisersModal;
+        },
+        isRequestResultModalShow() {
+            return this.requestResultModal;
+        }
+    },
+    methods: {
+        onPublicOfferClick(event) {
+            this.closeAllModal();
+            this.publicOfferModal = true;
+        },
+        onNeedAdvertisersClick() {
+            this.closeAllModal();
+            this.needAdvertisersModal = true;
+        },
+        onSendRequestClick() {
+            if (this.requestPending) {
+                return false;
+            }
+            // todo: Тут собственно необходимо сделать отправку запроса на бэкенд,
+            //       предварительно заблокировав кнопку отправки от повторного нажатия.
+            this.closeAllModal();
+            this.requestResultModal = true;
+        },
+        onJoinToRoomClick(room) {
+            return false;
+        },
+        closeAllModal() {
+            this.publicOfferModal = false;
+            this.needAdvertisersModal = false;
+            this.requestResultModal = false;
+        },
+        onCancelClick(modal) {
+            if (modal + 'Modal' in this) {
+                this[modal + 'Modal'] = false;
+            }
+        }
+    }
+});
+
+offersList.$on('public-offer-clicked', modals.onPublicOfferClick);
+offersList.$on('need-advertisers-clicked', modals.onNeedAdvertisersClick);
