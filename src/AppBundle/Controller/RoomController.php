060216e8e765ed9ac03c7f6f9498ae5860245ffc
Sergey Politsyn <spair2k@gmail.com>
2019-12-10 17:10:25 +0300

    адаптированы несколько полей, исправлен шаблон заголовков (промежуточный комит для ревью)

--- src/AppBundle/Controller/RoomController.php
+++ src/AppBundle/Controller/RoomController.php
@@ -85,6 +85,7 @@ class RoomController extends Controller
             $this->addFlash('success', 'Новая комната успешно создана');
 
             $eventDispatcher->dispatch(
+                //TODO: вываливает эксепшн при создании комнаты, если заходим компанией
                 RoomEvent::NEW_CREATED,
                 new RoomEvent($room)
             );
@@ -181,8 +182,8 @@ class RoomController extends Controller
             return $this->redirectToRoute('app_room_list');
         }
 
-        $leads = $this->entityManager->getRepository(Lead::class)
-            ->findBy(['room' => $room], ['createdAt' => 'DESC']);
+        //$leads = $this->entityManager->getRepository(Lead::class)
+        //    ->findBy(['room' => $room], ['createdAt' => 'DESC']);
 
         $buyers = $this->entityManager->getRepository(User::class)->getAdvertisersInRoom($room);
 
@@ -196,7 +197,7 @@ class RoomController extends Controller
 
         return $this->render('@App/v3/Room/view.html.twig', [
             'room' => $room,
-            'leads' => $leads,
+            //'leads' => $leads,
             'countCanBuy' => $this->roomManager->countCanBuy($room, $buyerFee, $totalAvailableMoney),
             'fee' => $feesManager->getCommissionForBuyerInRoom($room)
         ]);
