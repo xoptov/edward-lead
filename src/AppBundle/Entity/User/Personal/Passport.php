1d54aef2642b9a7b2c2ebe401da478b74b52d7f6
Maksim Khortov <xoptov@mail.ru>
2019-12-22 06:02:28 +0300

    Убрал сущность Company и вместо неё сделал встроенные объекты Organization, Office, Personal, Passport. Это получилось более ООП, но тперь повылазило куча проблем где нужно заменить Company на пользователя

--- src/AppBundle/Entity/User/Personal/Passport.php
+++ src/AppBundle/Entity/User/Personal/Passport.php
@@ -2,6 +2,12 @@
 
 namespace AppBundle\Entity\User\Personal;
 
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Embeddable
+ */
 class Passport
 {
     /**
@@ -15,7 +21,7 @@ class Passport
     private $issuer;
 
     /**
-     * @var \DateTime|null
+     * @var DateTime|null
      */
     private $issueDate;
 
@@ -23,4 +29,88 @@ class Passport
      * @var string|null
      */
     private $permanentAddress;
+
+    /**
+     * @return string|null
+     */
+    public function getSerialNumber(): ?string
+    {
+        return $this->serialNumber;
+    }
+
+    /**
+     * @param string|null $serialNumber
+     * 
+     * @return Passport
+     */
+    public function setSerialNumber(?string $serialNumber): self
+    {
+        $this->serialNumber = $serialNumber;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getIssuer(): ?string
+    {
+        return $this->issuer;
+    }
+
+    /**
+     * @param string|null $issuer
+     * 
+     * @return Passport
+     */
+    public function setIssuer(?string $issuer): self
+    {
+        $this->issuer = $issuer;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTime|null
+     */
+    public function getIssueDate(): ?DateTime
+    {
+        if ($this->issueDate) {
+            return clone $this->issueDate;
+        }
+
+        return $this->issueDate;
+    }
+
+    /**
+     * @param DateTime $issueDate
+     * 
+     * @return Passport
+     */
+    public function setIssueDate(DateTime $issueDate): self
+    {
+        $this->issueDate = $issueDate;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPermanentAddress(): ?string
+    {
+        return $this->permanentAddress;
+    }
+
+    /**
+     * @param string|null $permanentAddress
+     * 
+     * @return Passport
+     */
+    public function setPermanentAddress(?string $permanentAddress): self
+    {
+        $this->permanentAddress = $permanentAddress;
+
+        return $this;
+    }
 }
