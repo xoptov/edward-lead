d65aa1f0254bfc5561104b0c67178f6bb0c5baa8
Sergey Politsyn <spair2k@gmail.com>
2019-12-25 18:02:53 +0300

    добавлен шаблон v3 сообщения об успешной смене пароля

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -302,7 +302,8 @@ class SecurityController extends Controller
                 new UserEvent($user)
             );
 
-            return new Response('Пароль пользователя успешно изменен');
+            // return new Response('Пароль пользователя успешно изменен');
+            return $this->render('@App/v3/Security/password_change_success.html.twig');
         }
 
         return $this->render('@App/Security/password_resetting.html.twig', [
