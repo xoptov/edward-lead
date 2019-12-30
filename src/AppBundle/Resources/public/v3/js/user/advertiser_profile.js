70abc68e39a016fee2b3e8d14e86395be13e0405
Maksim Khortov <xoptov@mail.ru>
2019-12-30 16:23:58 +0300

    Исправил ошибки повылазившие в момент перезда на новые структуры данных

--- src/AppBundle/Resources/public/v3/js/user/advertiser_profile.js
+++ src/AppBundle/Resources/public/v3/js/user/advertiser_profile.js
@@ -1,3 +1,25 @@
+const CompanyComponent = {
+
+};
+
+const PersonalComponent = {
+
+};
+
 const advertiserProfile = new Vue({
-    el: '#advertiser-profile'
+    el: '#advertiser-profile',
+    components: {
+        'company-component': CompanyComponent,
+        'personal-component': PersonalComponent
+    },
+    data: {
+        user: null
+    },
+    mounted() {
+        const userId = this.$el.dataset.userId;
+        this.$http.get('/api/v1/user/me?deep=1')
+            .then(
+                resp => resp.json().then(data => this.user = data)
+            );
+    }
 });
